6ef6126be4e581161d2649f3c4f90d614eac9064
Rustam Magomed-Kasumov <rkasumov@mdcm.co>
2025-05-17 13:16:00 +0300

    Added user service and DI

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,22 +1,24 @@
 package com.example.demo.controller;
 
 import com.example.demo.repository.User;
+import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
     public List<User> helloWorld() {
-        return List.of(
-                new User(1L, "Sergey", 20, LocalDate.of(1996, 6, 1), "personal.email@mail.ru"),
-                new User(2L, "Ivan", 21, LocalDate.of(1997, 6, 1), "personal.email@mail.ru"),
-                new User(3L, "Sasha", 22, LocalDate.of(1998, 6, 1), "personal.email@mail.ru")
-        );
+        return userService.helloWorld();
     }
 }
